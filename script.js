8550591e221d21068944de0ea932b3feeeb3e37a
angulodaniel <angulo.daniel@hotmail.es>
2020-08-02 21:08:31 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+    $(".imagen-tarjeta").css("max-height", "250px");
+    $(".imagen-tarjeta").css("border-top-left-radius", "10px");
+    $(".imagen-tarjeta").css("border-top-right-radius", "10px");
+
+});
